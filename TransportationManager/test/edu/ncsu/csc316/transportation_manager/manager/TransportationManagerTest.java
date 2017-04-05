9d6478bdbe711e7da467b63dd990d4b5b7592c28
NBoar <NBoar@Leonard>
2017-04-05 15:57:58 -0400

    Made testing more proper in TransManagerTest

--- TransportationManager/test/edu/ncsu/csc316/transportation_manager/manager/TransportationManagerTest.java
+++ TransportationManager/test/edu/ncsu/csc316/transportation_manager/manager/TransportationManagerTest.java
@@ -19,12 +19,10 @@ public class TransportationManagerTest {
 		
 		TransportationManager tm = new TransportationManager("input/input2.txt");
 	
-		String expectedAdjList = tm.getAdjacencyList();
-		String expectedMinHighwaysC = tm.getMinimumHighways("COST");
-		String expectedMinHighwaysA = tm.getMinimumHighways("ASPHALT");
+		String expectedAdjList = "AdjacencyList[\n   City 0: -> Highway[city1=0, city2=3, cost=14.0, asphalt=415.0] -> Highway[city1=1, city2=0, cost=5.0, asphalt=99.0] -> Highway[city1=2, city2=0, cost=7.0, asphalt=159.0]\n   City 1: -> Highway[city1=1, city2=0, cost=5.0, asphalt=99.0] -> Highway[city1=1, city2=2, cost=6.0, asphalt=72.0] -> Highway[city1=3, city2=1, cost=10.0, asphalt=112.0]\n   City 2: -> Highway[city1=1, city2=2, cost=6.0, asphalt=72.0] -> Highway[city1=2, city2=0, cost=7.0, asphalt=159.0] -> Highway[city1=3, city2=2, cost=12.0, asphalt=212.0]\n   City 3: -> Highway[city1=0, city2=3, cost=14.0, asphalt=415.0] -> Highway[city1=3, city2=1, cost=10.0, asphalt=112.0] -> Highway[city1=3, city2=2, cost=12.0, asphalt=212.0]\n]";
+		String expectedMinHighwaysC = "List[\n   Highway[city1=1, city2=0, cost=5.0, asphalt=99.0],\n   Highway[city1=1, city2=2, cost=6.0, asphalt=72.0],\n   Highway[city1=3, city2=1, cost=10.0, asphalt=112.0]\n]";
 		
 		assertEquals(expectedAdjList, tm.getAdjacencyList());
 		assertEquals(expectedMinHighwaysC, tm.getMinimumHighways("COST"));
-		assertEquals(expectedMinHighwaysA, tm.getMinimumHighways("ASPHALT"));
 	}
 }
